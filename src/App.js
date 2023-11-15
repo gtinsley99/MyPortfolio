e0691a23d57ba8b1241008adcd22388cb57ff565
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-11-15 19:34:05 +0000

    add basic about me

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
 import Detail from './components/detail/Detail';
+import AboutMe from './components/aboutme/AboutMe';
 
 function App() {
   return (
     <div className="App">
         <Navbar />
         <Detail />
-        {/* <AboutMe />
-        <Techs />
+        <AboutMe />
+        {/* <Techs />
         <Projects /> */}
     </div>
   );
