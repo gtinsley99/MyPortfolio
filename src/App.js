a0d332447fccda0b57055b097098263686322908
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-11-15 19:21:50 +0000

    add basic detail component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Navbar from './components/navbar/Navbar';
+import Detail from './components/detail/Detail';
 
 function App() {
   return (
     <div className="App">
         <Navbar />
-        {/* <Detail />
-        <AboutMe />
+        <Detail />
+        {/* <AboutMe />
         <Techs />
         <Projects /> */}
     </div>
---
+++ src/components/detail/Detail.jsx
@@ -0,0 +1,15 @@
+import "./Detail.css";
+
+const Detail = () => {
+    return(
+        <div className="detail">
+            <div className="content">
+                <h2>Hey, I'm George</h2>
+                <p>I'm a full stack developer...</p>
+                <p>Links</p>
+            </div>
+        </div>
+    );
+};
+
+export default Detail;
