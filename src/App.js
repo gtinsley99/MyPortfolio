451a3af65cd1fe47de91a71725c93011730542f9
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-11-15 16:18:20 +0000

    add basic navbar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-        <NavBar />
-        <Detail />
+        <Navbar />
+        {/* <Detail />
         <AboutMe />
         <Techs />
-        <Projects />
+        <Projects /> */}
     </div>
   );
 }
