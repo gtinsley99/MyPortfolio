a6f41ceb2734e2f68abb5ca7c6e50ca49b633ea6
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-11-24 14:25:59 +0000

    add new background image detail

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,15 +1,22 @@
 import "./Detail.css";
 
 const Detail = () => {
-    return(
-        <div className="detail">
-            <div className="content">
-                <h2>Hey, I'm George</h2>
-                <p>I'm a full stack developer...</p>
-                <p>Links</p>
-            </div>
+  return (
+    <div className="detail">
+      <div className="content">
+        <div className="greet">
+          <h2>Hey, I'm George</h2>
         </div>
-    );
+        <div className="info">
+          <p>
+            I'm a full stack developer with a passion for building responsive
+            websites and REST APIs.
+          </p>
+          <p>Links</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Detail;
