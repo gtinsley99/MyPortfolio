e9de84df0412df041e8def21c36c806ca29f8e98
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-11-24 15:03:01 +0000

    add images and links to socials

--- src/components/detail/Detail.jsx
+++ src/components/detail/Detail.jsx
@@ -1,4 +1,8 @@
 import "./Detail.css";
+import github from "../../images/github-mark-c791e9551fe4/github-mark/github-mark-white.svg";
+import linkedin from "../../images/LinkedIn_icon.svg.png";
+import CV from "../../static/GeorgeTinsleyCV.pdf";
+import pdf from "../../images/pdf.svg";
 
 const Detail = () => {
   return (
@@ -12,7 +16,23 @@ const Detail = () => {
             I'm a full stack developer with a passion for building responsive
             websites and REST APIs.
           </p>
-          <p>Links</p>
+          <div className="socials">
+            <span>
+              <a href="https://github.com/gtinsley99" target="_blank">
+                <img src={github} alt="GitHub"/>
+              </a>
+            </span>
+            <span>
+                <a href="https://www.linkedin.com/in/george-tinsley-05047217b/" target="_blank">
+                    <img src={linkedin} width={100} alt="LinkedIn"/>
+                </a>
+            </span>
+            <span>
+                <a href={CV} target="_blank">
+                    <img src={pdf} width={100} alt="CV"/>
+                </a>
+            </span>
+          </div>
         </div>
       </div>
     </div>
